a329fafcb5fe6dfd530d7f5c58b16d41f27346cd
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-29 20:08:54 +0530

    user controllers declarations added

--- backend/src/entities/user/user.controllers.js
+++ backend/src/entities/user/user.controllers.js
@@ -1,5 +1,3 @@
-import { asyncHandler } from "../../utils/async-handler.js";
-
 export const registerUser = asyncHandler(async (req, res) => {});
 
 export const loginUser = asyncHandler(async (req, res) => {});
