d0b14327f00634392e79e18b23a65a889202342f
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-29 20:02:46 +0530

    project controllers declarations added

---
+++ backend/src/entities/project/project.controllers.js
@@ -0,0 +1,17 @@
+export const getProjects = async (req, res) => {};
+
+export const getProjectById = async (req, res) => {};
+
+export const createProject = async (req, res) => {};
+
+export const updateProject = async (req, res) => {};
+
+export const deleteProject = async (req, res) => {};
+
+export const getProjectMembers = async (req, res) => {};
+
+export const addMemberToProject = async (req, res) => {};
+
+export const deleteMemberFromProject = async (req, res) => {};
+
+export const updateMemberRole = async (req, res) => {};
