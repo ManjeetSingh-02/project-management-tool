ef7691f26770d98126e186ade5a5ca30c2ca48b5
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-29 20:04:46 +0530

    project controllers declarations added

---
+++ backend/src/entities/user/user.controllers.js
@@ -0,0 +1,21 @@
+import { asyncHandler } from "../../utils/async-handler.js";
+
+export const registerUser = asyncHandler(async (req, res) => {});
+
+export const loginUser = asyncHandler(async (req, res) => {});
+
+export const logoutUser = asyncHandler(async (req, res) => {});
+
+export const verifyEmail = asyncHandler(async (req, res) => {});
+
+export const resendEmailVerification = asyncHandler(async (req, res) => {});
+
+export const forgotPasswordRequest = asyncHandler(async (req, res) => {});
+
+export const resetForgottenPassword = asyncHandler(async (req, res) => {});
+
+export const changeCurrentPassword = asyncHandler(async (req, res) => {});
+
+export const getCurrentUser = asyncHandler(async (req, res) => {});
+
+export const refreshAccessToken = asyncHandler(async (req, res) => {});
