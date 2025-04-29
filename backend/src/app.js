454b0ff23f291cdb3909df6bcdbfc8117554e68c
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-29 19:48:55 +0530

    project router added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { healthCheckRouter, noteRouter } from "./entities/index.js";
+import { healthCheckRouter, noteRouter, projectRouter } from "./entities/index.js";
 
 const app = express();
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/notes", noteRouter);
+app.use("/api/v1/projects", projectRouter);
 
 export default app;
--- backend/src/entities/index.js
+++ backend/src/entities/index.js
@@ -1,2 +1,3 @@
 export { default as healthCheckRouter } from "./healtcheck/healthcheck.routes.js";
 export { default as noteRouter } from "./note/note.routes.js";
+export { default as projectRouter } from "./project/project.routes.js";
