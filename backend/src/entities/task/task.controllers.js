7102f88eab038982902e123e28a8ff641b71e62c
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-29 20:04:14 +0530

    task controllers declarations added

---
+++ backend/src/entities/task/task.controllers.js
@@ -0,0 +1,15 @@
+export const getTasks = async (req, res) => {};
+
+export const getTaskById = async (req, res) => {};
+
+export const createTask = async (req, res) => {};
+
+export const updateTask = async (req, res) => {};
+
+export const deleteTask = async (req, res) => {};
+
+export const createSubTask = async (req, res) => {};
+
+export const updateSubTask = async (req, res) => {};
+
+export const deleteSubTask = async (req, res) => {};
