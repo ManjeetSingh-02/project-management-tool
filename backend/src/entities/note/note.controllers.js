342afda2a376b59b8665d86e91dc57d14b1fe3a6
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-29 20:02:22 +0530

    note controllers declarations added

---
+++ backend/src/entities/note/note.controllers.js
@@ -0,0 +1,9 @@
+export const getNotes = async (req, res) => {};
+
+export const getNoteById = async (req, res) => {};
+
+export const createNote = async (req, res) => {};
+
+export const updateNote = async (req, res) => {};
+
+export const deleteNote = async (req, res) => {};
