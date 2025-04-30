d34e20f0cfbd78fccd2aa3476438eea24718d446
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-30 20:38:24 +0530

    changed data field

--- backend/src/entities/healtcheck/healthcheck.controllers.js
+++ backend/src/entities/healtcheck/healthcheck.controllers.js
@@ -1,5 +1,5 @@
 import { APIResponse } from "../../utils/api/apiResponse.js";
 
 export const healthCheck = (_, res) => {
-  res.status(200).json(new APIResponse(200, { message: "Server is Running" }));
+  res.status(200).json(new APIResponse(200, "Server is Running"));
 };
