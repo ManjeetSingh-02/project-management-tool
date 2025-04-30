520359b17e686c7e8ac8f7e58d20d619d4fe5184
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-30 22:34:37 +0530

    improved error handling

--- backend/src/utils/db/index.js
+++ backend/src/utils/db/index.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 // function to connect to the database
 export async function connectToDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected with DB");
-  } catch (error) {
-    console.error("Connection with DB failed, err -> ", error);
-    process.exit(1);
-  }
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected with DB"))
+    .catch(error => {
+      console.log("Error connecting to DB", error);
+      process.exit(1);
+    });
 }
