2651462a268a65865901707e0ef3aea1125aca68
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-04-30 22:28:00 +0530

    improved error response

--- backend/src/utils/async-handler.js
+++ backend/src/utils/async-handler.js
@@ -1,6 +1,12 @@
 // wrapper function to catch errors
 export function asyncHandler(requestHandler) {
   return function (req, res, next) {
-    Promise.resolve(requestHandler(req, res, next)).catch(err => next(err));
+    Promise.resolve(requestHandler(req, res, next)).catch(err =>
+      res.status(err.statusCode).json({
+        statusCode: err.statusCode,
+        message: err.message,
+        error: err.errors,
+      }),
+    );
   };
 }
