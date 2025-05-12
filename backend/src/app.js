8142f972e13a73c42bef8950450727154e8b1ca2
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-05-13 02:02:59 +0530

    update CORS middleware to include PATCH method in allowed HTTP methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: process.env.ORIGIN_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
     exposedHeaders: ["Set-Cookie", "*"],
   }),
