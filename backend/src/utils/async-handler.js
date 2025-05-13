f6b19854e14e00dc81bd1577fc87067951ff68f3
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-05-13 21:47:19 +0530

    fix: improve error handling in asyncHandler utility

--- backend/src/utils/async-handler.js
+++ backend/src/utils/async-handler.js
@@ -2,10 +2,10 @@
 export function asyncHandler(requestHandler) {
   return function (req, res, next) {
     Promise.resolve(requestHandler(req, res, next)).catch(err =>
-      res.status(err.statusCode).json({
-        statusCode: err.statusCode,
-        message: err.message,
-        error: err.errors,
+      res.status(err.statusCode ?? 500).json({
+        statusCode: err.statusCode ?? 500,
+        message: err.message ?? "Internal Server Error",
+        error: err.errors ?? err.error ?? "Something went wrong",
       }),
     );
   };
