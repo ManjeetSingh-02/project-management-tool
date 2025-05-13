4d258dff9bbf12a415533f076bd7183d7e00a4ff
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-05-13 11:55:30 +0530

    projects routes refactored for better usage and projectmember routes added

--- backend/src/entities/project/project.routes.js
+++ backend/src/entities/project/project.routes.js
@@ -5,6 +5,10 @@ import {
   getProjectById,
   updateProject,
   deleteProject,
+  getProjectMembers,
+  addMemberToProject,
+  deleteMemberFromProject,
+  updateMemberRole,
 } from "./project.controllers.js";
 import { projectCreationValidator } from "../../utils/validator/projectValidators.js";
 import { validate } from "../../utils/validator/index.js";
@@ -12,10 +16,14 @@ import { isLoggedIn } from "../../utils/route-protector.js";
 
 const router = Router();
 
-router.post("/create", isLoggedIn, projectCreationValidator(), validate, createProject);
-router.get("/getprojects", isLoggedIn, getProjects);
-router.get("/getprojects/:id", isLoggedIn, getProjectById);
-router.patch("/update/:id", isLoggedIn, projectCreationValidator(), validate, updateProject);
-router.delete("/delete/:id", isLoggedIn, deleteProject);
+router.get("/", isLoggedIn, getProjects);
+router.get("/:id", isLoggedIn, getProjectById);
+router.post("/", isLoggedIn, projectCreationValidator(), validate, createProject);
+router.patch("/:id", isLoggedIn, projectCreationValidator(), validate, updateProject);
+router.delete("/:id", isLoggedIn, deleteProject);
+router.get("/:id/members", isLoggedIn, getProjectMembers);
+router.post("/:id/members", isLoggedIn, addMemberToProject);
+router.patch("/:id/members/:memberId", isLoggedIn, updateMemberRole);
+router.delete("/:id/members/:memberId", isLoggedIn, deleteMemberFromProject);
 
 export default router;
