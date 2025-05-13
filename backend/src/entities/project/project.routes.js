e0ecdbad58e15fd22918900c3375a7fd2b49946e
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-05-14 00:08:29 +0530

    refactor: enhance project and member validation logic in routes and validators

--- backend/src/entities/project/project.routes.js
+++ backend/src/entities/project/project.routes.js
@@ -10,20 +10,49 @@ import {
   deleteMemberFromProject,
   updateMemberRole,
 } from "./project.controllers.js";
-import { projectCreationValidator } from "../../utils/validator/projectValidators.js";
+import {
+  projectValidator,
+  projectIdValidator,
+  projectMemberIdValidator,
+  projectMemberValidator,
+  projectMemberRoleValidator,
+} from "../../utils/validator/projectValidators.js";
 import { validate } from "../../utils/validator/index.js";
 import { isLoggedIn } from "../../utils/route-protector.js";
 
 const router = Router();
 
 router.get("/", isLoggedIn, getProjects);
-router.get("/:id", isLoggedIn, getProjectById);
-router.post("/", isLoggedIn, projectCreationValidator(), validate, createProject);
-router.patch("/:id", isLoggedIn, projectCreationValidator(), validate, updateProject);
-router.delete("/:id", isLoggedIn, deleteProject);
-router.get("/:id/members", isLoggedIn, getProjectMembers);
-router.post("/:id/members", isLoggedIn, addMemberToProject);
-router.patch("/:id/members/:memberId", isLoggedIn, updateMemberRole);
-router.delete("/:id/members/:memberId", isLoggedIn, deleteMemberFromProject);
+router.get("/:id", isLoggedIn, projectIdValidator(), validate, getProjectById);
+router.post("/", isLoggedIn, projectValidator(), validate, createProject);
+router.patch("/:id", isLoggedIn, projectIdValidator(), projectValidator(), validate, updateProject);
+router.delete("/:id", isLoggedIn, projectIdValidator(), validate, deleteProject);
+
+router.get("/:id/members", isLoggedIn, projectIdValidator(), validate, getProjectMembers);
+router.post(
+  "/:id/members",
+  isLoggedIn,
+  projectIdValidator(),
+  projectMemberValidator(),
+  validate,
+  addMemberToProject,
+);
+router.patch(
+  "/:id/members/:memberId",
+  isLoggedIn,
+  projectIdValidator(),
+  projectMemberIdValidator(),
+  projectMemberRoleValidator(),
+  validate,
+  updateMemberRole,
+);
+router.delete(
+  "/:id/members/:memberId",
+  isLoggedIn,
+  projectIdValidator(),
+  projectMemberIdValidator(),
+  validate,
+  deleteMemberFromProject,
+);
 
 export default router;
