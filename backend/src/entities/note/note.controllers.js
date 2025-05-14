f65b64a83e923a81d18edbbe8eac9a67feb606ec
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-05-15 01:10:59 +0530

    getNotes controller and route completed

--- backend/src/entities/note/note.controllers.js
+++ backend/src/entities/note/note.controllers.js
@@ -1,4 +1,20 @@
-export const getNotes = async (req, res) => {};
+import { asyncHandler } from "../../utils/async-handler.js";
+import { APIError } from "../../utils/api/apiError.js";
+import { APIResponse } from "../../utils/api/apiResponse.js";
+import { ProjectNote } from "./note.models.js";
+
+export const getNotes = asyncHandler(async (req, res) => {
+  // get id from params
+  const { id } = req.params;
+
+  // get all project notes
+  const allNotes = await ProjectNote.find({ project: id }).select("-createdAt -updatedAt -__v");
+  if (!allNotes.length)
+    throw new APIError(400, "Get All Notes Error", "No notes found for this project");
+
+  // success status to user
+  return res.status(200).json(new APIResponse(200, "Notes fetched successfully", allNotes));
+});
 
 export const getNoteById = async (req, res) => {};
 
