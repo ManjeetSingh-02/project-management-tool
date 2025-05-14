8bec7ae4770cc6b22afee6e9733bd3933fd27f30
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-05-14 16:14:37 +0530

    feat: implement role-based access control for project members

--- backend/src/utils/route-protector.js
+++ backend/src/utils/route-protector.js
@@ -2,6 +2,7 @@ import { APIError } from "./api/apiError.js";
 import { asyncHandler } from "./async-handler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../entities/user/user.models.js";
+import { ProjectMember } from "../entities/project/projectmember.models.js";
 
 // function to check for any validation errors
 export const isLoggedIn = asyncHandler(async (req, _, next) => {
@@ -25,3 +26,24 @@ export const isLoggedIn = asyncHandler(async (req, _, next) => {
   // forward request to next middleware
   next();
 });
+
+// function for role based access control
+export const hasRolePermission = (roles = []) =>
+  asyncHandler(async (req, _, next) => {
+    // get id from params
+    const { id } = req.params;
+
+    // check if project member exists
+    const existingMember = await ProjectMember.findOne({ user: req.user.id, project: id });
+    if (!existingMember) throw new APIError(400, "Security Error", "Access Denied");
+
+    // check if user has permission
+    if (!roles.includes(existingMember.role))
+      throw new APIError(403, "Security Error", "Access Denied for this action");
+
+    // set user role in request object
+    req.user.role = existingMember.role;
+
+    // forward request to next middleware
+    next();
+  });
