1fca5db42d17156c1e7d82e0b0f61789eaab863f
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-05-21 23:50:21 +0530

    feat: enhance task validation logic with comprehensive checks for title, description, assigned user ID, and optional attachments

--- backend/src/utils/validator/task.validators.js
+++ backend/src/utils/validator/task.validators.js
@@ -1,4 +1,35 @@
+import { body, param } from "express-validator";
+
 // function to check for task id validation errors
 export const taskIdValidator = () => {
   return [param("taskId").trim().isMongoId().withMessage("task id is invalid")];
+};
+
+// function check for task validation errors
+export const taskValidator = () => {
+  return [
+    body("title")
+      .trim()
+      .notEmpty()
+      .withMessage("task title is required")
+      .isLength({ min: 3 })
+      .withMessage("task name should be atleast 3 chars")
+      .isLength({ max: 13 })
+      .withMessage("task name should not be more than 13 chars"),
+    body("description")
+      .trim()
+      .notEmpty()
+      .withMessage("task description is required")
+      .isLength({ min: 10 })
+      .withMessage("task description should be atleast 10 chars")
+      .isLength({ max: 100 })
+      .withMessage("task description should not be more than 100 chars"),
+    body("assignedToUserId")
+      .trim()
+      .notEmpty()
+      .withMessage("assigned to user id is required")
+      .isMongoId()
+      .withMessage("invalid user id"),
+    body("attachments").optional().isArray().withMessage("attachments should be an array"),
+  ];
 };
